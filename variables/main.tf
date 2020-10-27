ffeeb2801f0ba585ee0905b63490e62a9369d8a1
DannyDenver <djt09d@fulbrightmail.org>
2020-10-27 12:55:55 -0600

    commit variables

---
+++ variables/main.tf
@@ -0,0 +1,26 @@
+variable "vpcname" {
+    type = string
+    default = "myvpc"
+}
+
+variable "sshport" {
+    type = number
+    default = 22
+}
+
+variable "enabled" {
+    default = true
+}
+
+variable "mylist" {
+    type = list(string)
+    default = ["Value1", "Value2"]
+}
+
+variable "mymap" {
+    type = map
+    default = {
+        Key1 = "value1"
+        Key2 = "value2"
+    }
+}
